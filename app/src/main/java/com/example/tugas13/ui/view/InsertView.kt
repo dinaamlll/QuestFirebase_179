2add9a66df8a0dd5ade662026baadc28a5e02cfd
dinaamlll <dinamaliaa17@gmail.com>
2025-01-10 21:22:17 +0700

    Menambahkan uiState, uiEvent snackbarHostState, dan coroutineScope

--- app/src/main/java/com/example/tugas13/ui/view/InsertView.kt
+++ app/src/main/java/com/example/tugas13/ui/view/InsertView.kt
@@ -1,9 +1,14 @@
 package com.example.tugas13.ui.view
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tugas13.ui.viewmodel.InsertViewModel
-import java.lang.reflect.Modifier
+import com.example.tugas13.ui.viewmodel.PenyediaViewModel
+
 
 @Composable
 fun InsertMhsView(
@@ -12,5 +17,9 @@ fun InsertMhsView(
     modifier: Modifier = Modifier,
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val uiState = viewModel.uiState //State utama untuk loading, success, error
+    val uiEvent = viewModel.uiEvent // State untuk form dan validasi
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
 }

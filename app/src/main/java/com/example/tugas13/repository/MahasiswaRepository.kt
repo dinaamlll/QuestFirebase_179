b8fed8b002a51382a9f910eb24e5a42737d07b48
dinaamlll <dinamaliaa17@gmail.com>
2025-01-10 17:47:16 +0700

    membenarkan kode yang salah

--- app/src/main/java/com/example/tugas13/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/tugas13/repository/MahasiswaRepository.kt
@@ -4,9 +4,9 @@ import com.example.tugas13.model.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
 interface MahasiswaRepository {
-    suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
-    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
-    suspend fun deleteMahasiswa(nim: String)
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
     suspend fun getMahasiswabyNim(nim: String): Flow <Mahasiswa>
 }

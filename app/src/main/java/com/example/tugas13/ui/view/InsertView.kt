1f3f51c537652bd1cae39c821d49a0c7d50c4129
dinaamlll <dinamaliaa17@gmail.com>
2025-01-10 21:48:47 +0700

    Menambahkan fungsi InsertBodyMhs dengan validasi, tombol submit, dan indikator loading.

--- app/src/main/java/com/example/tugas13/ui/view/InsertView.kt
+++ app/src/main/java/com/example/tugas13/ui/view/InsertView.kt
@@ -1,9 +1,13 @@
 package com.example.tugas13.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -13,11 +17,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tugas13.ui.viewmodel.FormState
+import com.example.tugas13.ui.viewmodel.InsertUiState
 import com.example.tugas13.ui.viewmodel.InsertViewModel
+import com.example.tugas13.ui.viewmodel.MahasiswaEvent
 import com.example.tugas13.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -36,7 +44,7 @@ fun InsertMhsView(
     val coroutineScope = rememberCoroutineScope()
 
 //observasi perubahan state untuk snackbar dan navigasi
-    LaunchedEffect (uiState) {
+    LaunchedEffect(uiState) {
         when (uiState) {
             is FormState.Success -> {
                 println(
@@ -50,15 +58,17 @@ fun InsertMhsView(
                 onNavigate()
                 viewModel.resetSnackBarMessage() //Reset snackbar state
             }
+
             is FormState.Error -> {
                 coroutineScope.launch {
                     snackbarHostState.showSnackbar(uiState.message)
                 }
             }
+
             else -> Unit
         }
     }
-    Scaffold (
+    Scaffold(
         modifier = modifier,
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
@@ -71,17 +81,17 @@ fun InsertMhsView(
                 }
             )
         }
-    ){ padding ->
-        Column (
+    ) { padding ->
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(padding)
                 .padding(16.dp)
-        ){
+        ) {
             InsertBodyMhs(
                 uiState = uiEvent,
                 homeUiState = uiState,
-                onValueChange = {updateEvent ->
+                onValueChange = { updateEvent ->
                     viewModel.updateState(updateEvent)
                 },
                 onClick = {
@@ -93,5 +103,43 @@ fun InsertMhsView(
             )
         }
     }
-
+}
+    @Composable
+    fun InsertBodyMhs(
+        modifier: Modifier = Modifier,
+        onValueChange: (MahasiswaEvent) -> Unit,
+        uiState: InsertUiState,
+        onClick: () -> Unit,
+        homeUiState: FormState
+    ){
+        Column (
+            modifier = modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            FormMahasiswa(
+                mahasiswaEvent = uiState.insertUiEvent,
+                onValueChange = onValueChange,
+                errorState = uiState.isEntryValid,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Button(
+                onClick = onClick,
+                modifier = Modifier.fillMaxWidth(),
+                enabled = homeUiState !is FormState.Loading,
+            ) {
+                if (homeUiState is FormState.Loading) {
+                    CircularProgressIndicator(
+                        color = Color.White,
+                        modifier = Modifier
+                            .size(20.dp)
+                            .padding(end = 8.dp)
+                    )
+                    Text("Loading...")
+                }else {
+                    Text("Add")
+                }
+            }
+        }
+    }
 }

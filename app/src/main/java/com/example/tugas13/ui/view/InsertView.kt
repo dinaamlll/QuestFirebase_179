d63f9494bd8db0673ce9c3f12b77838ad90dbd25
dinaamlll <dinamaliaa17@gmail.com>
2025-01-10 21:28:44 +0700

    Menambahkan LaunchedEffect untuk menampilkan snackbar dan navigasi

--- app/src/main/java/com/example/tugas13/ui/view/InsertView.kt
+++ app/src/main/java/com/example/tugas13/ui/view/InsertView.kt
@@ -2,12 +2,16 @@ package com.example.tugas13.ui.view
 
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tugas13.ui.viewmodel.FormState
 import com.example.tugas13.ui.viewmodel.InsertViewModel
 import com.example.tugas13.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -22,4 +26,27 @@ fun InsertMhsView(
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
 
+//observasi perubahan state untuk snackbar dan navigasi
+    LaunchedEffect (uiState) {
+        when (uiState) {
+            is FormState.Success -> {
+                println(
+                    "InsertMhsView: uiState is FormState.Success, navigate to home" + uiState.message
+                )
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message) //Tampilkan snackbar
+                }
+                delay(700)
+                //Navigasi Langsung
+                onNavigate()
+                viewModel.resetSnackBarMessage() //Reset snackbar state
+            }
+            is FormState.Error -> {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+            }
+            else -> Unit
+        }
+    }
 }

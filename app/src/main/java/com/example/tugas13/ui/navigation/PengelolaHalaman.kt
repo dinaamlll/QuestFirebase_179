fe799a7f3722585f1d04f65a0acdce4d16a23119
dinaamlll <dinamaliaa17@gmail.com>
2025-01-11 20:11:16 +0700

    Menambahkan fungsi PengelolaHalaman untuk mengatur navigasi antar layar HomeScreen dan InsertMhsView

---
+++ app/src/main/java/com/example/tugas13/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,38 @@
+package com.example.tugas13.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.tugas13.ui.view.HomeScreen
+import com.example.tugas13.ui.view.InsertMhsView
+
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+        composable(DestinasiInsert.route) {
+            InsertMhsView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
